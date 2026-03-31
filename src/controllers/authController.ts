c23ddfbd3f845c418d0ca6d435107aad376e55e7
GeorgeLxL <geolxl@hotmail.com>
2026-03-31 14:38:03 +0900

    console.log for debug

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -30,6 +30,8 @@ export async function callback(req: Request, res: Response) {
 
     const tokenRes = await axios.post(SMAREGI_TOKEN_URL!, params.toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
 
+    console.log('Token response:', tokenRes, tokenRes.data); // 追加: トークンレスポンスをログに出力
+
     const { access_token } = tokenRes.data;
 
     const userRes = await axios.get(`${SMAREGI_API_BASE}/userinfo`, {
